ae2eb2723e7a15358f977a0929eb778091a29275
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2025-05-07 18:19:45 +0800

    CORS Changes

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ from typing import List, Dict
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://autocorrection-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
